3a23f81f8a1e24b62c9b565cfcfeb0be223c5b64
gebeen-dev <rrdd1207@gmail.com>
2025-02-19 14:36:52 +0900

    Initial commit

---
+++ src/main/java/com/lessonhub/LessonHubApplication.java
@@ -0,0 +1,13 @@
+package com.lessonhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LessonHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lessonhub/LessonHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lessonhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
