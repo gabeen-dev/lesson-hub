9317965bdf516028034dcbccbb4178fcc486662f
gebeen-dev <rrdd1207@gmail.com>
2025-02-25 12:37:47 +0900

    feat : Timestamped bseentity 값 추가

--- src/main/java/com/lessonhub/entity/Timestamped.java
+++ src/main/java/com/lessonhub/entity/Timestamped.java
@@ -1,4 +1,26 @@
 package com.lessonhub.entity;
 
-public class Timestamped {
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+
+	@CreatedDate
+	@Column(updatable = false, nullable = false)
+	private LocalDateTime createdDate;
+
+	@LastModifiedDate
+	@Column(nullable = false)
+	private LocalDateTime modifiedDate;
+
 }
