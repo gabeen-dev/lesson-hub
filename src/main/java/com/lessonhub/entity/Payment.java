6c00edcc4586d7510065ecd7b77331844e6fa879
gebeen-dev <rrdd1207@gmail.com>
2025-02-25 13:56:33 +0900

    feat : Payment 필드 추가

--- src/main/java/com/lessonhub/entity/Payment.java
+++ src/main/java/com/lessonhub/entity/Payment.java
@@ -1,4 +1,28 @@
 package com.lessonhub.entity;
 
-public class Payment {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Payment extends Timestamped {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false)
+	private int totalPrice;
+
+	@Column(nullable = false)
+	private int basePrice;
+
+	@Column
+	private String paymentType;
+
+	@Column(nullable = false, length = 100)
+	private String status;
+
 }
