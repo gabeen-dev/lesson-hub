79663db33fbabb4821eb0f988330e87592c3b0c1
gebeen-dev <rrdd1207@gmail.com>
2025-02-25 13:56:10 +0900

    feat : PraticeRoom 필드 추가

--- src/main/java/com/lessonhub/entity/PracticeRoom.java
+++ src/main/java/com/lessonhub/entity/PracticeRoom.java
@@ -1,4 +1,22 @@
 package com.lessonhub.entity;
 
-public class PracticeRoom {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class PracticeRoom extends Timestamped {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, length = 11)
+	private int price;
+
+	@Column(nullable = false, length = 100)
+	private String state;
+
 }
