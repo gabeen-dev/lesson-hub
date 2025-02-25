510e96e6e42da6d637b13c64337c4e29d24e1ee6
gebeen-dev <rrdd1207@gmail.com>
2025-02-25 13:54:41 +0900

    feat : Users 필드 추가

--- src/main/java/com/lessonhub/entity/Users.java
+++ src/main/java/com/lessonhub/entity/Users.java
@@ -1,4 +1,31 @@
 package com.lessonhub.entity;
 
-public class Users {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Users extends Timestamped {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false, length = 50)
+	private String name;
+
+	@Column(nullable = false, length = 100)
+	private String role;
+
+	@Column(nullable = false, length = 100)
+	private String status;
+
 }
