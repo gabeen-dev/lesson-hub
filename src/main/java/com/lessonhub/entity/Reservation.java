c190e3369036a81167b2b94971b1ec1e10d7c58a
gebeen-dev <rrdd1207@gmail.com>
2025-02-25 13:55:29 +0900

    feat : Reservation 필드 추가

--- src/main/java/com/lessonhub/entity/Reservation.java
+++ src/main/java/com/lessonhub/entity/Reservation.java
@@ -1,4 +1,33 @@
 package com.lessonhub.entity;
 
-public class Reservation {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Reservation extends Timestamped {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private LocalDate reservationStartTime;
+
+	@Column(nullable = false)
+	private LocalDate reservationEndTime;
+
+	@Column
+	private LocalDate canceledDate;
+
+	@Column
+	private String requests;
+
+	@Column(nullable = false, length = 100)
+	private String status;
+
 }
